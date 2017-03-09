ad6b70c7ff3b5166fc023bac76db8d813114c4b4
Daria Mamaeva <daria_mamaeva@hotmail.com>
2017-03-09 19:39:18 +0300

    Delete first task: anagram

--- frontend-fintech/Task1/app.js
+++
@@ -1,5 +0,0 @@
-function checkAn() {
-    let first = prompt("First word", "");
-    let second = prompt("Second word", "");
-    alert(first.split('').sort().join('')===second.split('').sort().join(''));
-}
